95636c4bf198589f7bcb07c29247b7d1bd1408f9
Krete <krete121@gmail.com>
2023-01-29 16:33:23 +0200

    add week2 code

---
+++ Week2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, world!");
+//uued andmetüübid: int ja char
+//if-statements (control flow)
+//rakendus küsib kasutajal tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi] / "Welcome, Ms. [kasutajanimi]"
+
+
